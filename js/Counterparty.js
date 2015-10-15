4de32f2a34e122454e558ddb7eb643e1bed7c29d
Alexander Bienz <abienz@gmail.com>
2015-10-15 16:38:01 +0100

    Added list and arrow keystrokes

--- js/Counterparty.js
+++ js/Counterparty.js
@@ -1,35 +1,72 @@
 var Counterparty = {
 	item_list: '',
 	item_filter: '',
-	API_URL: '/json/Counterparty.json',
+	API_URL: '/json/counterparty.json',
+	RECENT_API_URL: '/json/recent_counterparty.json',
 	json_data: '',
+	recent_data: '',
 
 	init: function(){
-		this.item_list = $('.list dl');
+		this.item_list = $('.list');
 		this.item_filter = $('#counterparty-text');
 		this.populateList();
 		this.addEvents();
 	},
 
 	populateList: function(){
-		var self = this;
+		var self = this,
+				all_list = self.item_list.find('.all'),
+				recent_list = self.item_list.find('.recent');
 
 		$.getJSON(self.API_URL, function(data){
+			data.sort(function(a, b){
+				if (a.company > b.company) {
+			    return 1;
+			  }
+			  if (a.company < b.company) {
+			    return -1;
+			  }
+			  return 0;
+			});
 			self.json_data = data;
+
 			$.each(data, function(index, item){
-				self.item_list.append('<dd data-item-id="' + item._id + '">' + item.company + '</dd>');
+				var gfx_code = item._id.substr(item._id.length - 6).toUpperCase();
+
+				all_list.append('<dd data-item-id="' + item._id + '">' + item.company + ' <span>' + gfx_code +'</span></dd>');
+			});
+		});
+
+		$.getJSON(self.RECENT_API_URL, function(data){
+			data.sort(function(a, b){
+				if (a.company > b.company) {
+			    return 1;
+			  }
+			  if (a.company < b.company) {
+			    return -1;
+			  }
+			  return 0;
+			});
+			self.recent_data = data;
+
+			$.each(data, function(index, item){
+				var gfx_code = item._id.substr(item._id.length - 6).toUpperCase();
+
+				recent_list.append('<dd data-item-id="' + item._id + '">' + item.company + ' <span>' + gfx_code +'</span></dd>');
 			});
 		});
 	},
 
 	addEvents: function(){
-		var self = this;
+		var self = this,
+				all_list = self.item_list.find('.all'),
+				recent_list = self.item_list.find('.recent');
 
 		$(document.body).on('click', self.item_list.find('dd'), function(e){
 			var clicked_item,
 					item_id;
 
-			clicked_item = $(e.target);
+			clicked_item = ($(e.target).is('dd')) ? $(e.target) : $(e.target).parents('dd') ;
 			item_id = clicked_item.data('item-id');
 			
 			self.item_list.find('dd').removeClass('active');
@@ -37,19 +74,55 @@ var Counterparty = {
 		});
 
 		self.item_filter.on('keyup', function(){
-			var new_data = self.json_data.filter(function(obj){
-				if (obj.title.indexOf(self.item_filter.val()) > -1) {
-					return true;
-				} else {
-					return false;
-				};
-			});
-			self.item_list.empty();
-			$.each(new_data, function(index, item){
-				var item_title = item.title.replace(self.item_filter.val(), '<span class="text-primary">' + self.item_filter.val() + '</span>');
-				self.item_list.append('<a href="#" class="list-group-item" data-item-id="' + item.id + '">' + item_title + '</a>');
-			});
+			var new_data = self.filterData(self.json_data),
+					new_recent_data = self.filterData(self.recent_data);
+
+			self.updateList(new_data, all_list);
+			self.updateList(new_recent_data, recent_list);
 		});
+
+		$(document.body).on('keyup, keydown', function(e){
+			if (e.keyCode == 38 || e.keyCode == 40){
+				self.handleArrows(e.keyCode);
+			}
+		});
+	},
+
+	filterData: function(data) {
+		var self = this,
+				filtered_data = '';
+
+		filtered_data = data.filter(function(obj){
+			if (obj.company.toUpperCase().indexOf(self.item_filter.val().toUpperCase()) > -1) {
+				return true;
+			} else {
+				return false;
+			};
+		});
+
+		return filtered_data;
+	},
+
+	updateList: function(data, list) {
+		var self = this;
+
+		list.find('dd').remove();
+
+		$.each(data, function(index, item){
+			var item_company = item.company.replace(self.item_filter.val(), '<strong>' + self.item_filter.val() + '</strong>'),
+					gfx_code = item._id.substr(item._id.length - 6).toUpperCase();
+
+			list.append('<dd data-item-id="' + item._id + '">' + item_company + ' <span>' + gfx_code + '</span></dd>');
+		});
+	},
+
+	handleArrows: function(key) {
+		var self = this,
+				is_list_open = self.item_list.hasClass('open')
+
+		if (is_list_open) {
+			console.log(key);
+		};
 	}
 }
 
